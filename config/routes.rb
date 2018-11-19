84d4674d425f819835cbe7e4df5a7e17e516f371
sarzynski <kamil.s.sarzynski@gmail.com>
2018-11-19 13:10:15 +0100

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :films
   resources :customers
   resources :screenings
-  root 'screenings#index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
