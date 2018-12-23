d8d5730757c43e50d80d06047281f71867ab45f3
sarzynski <kamil.s.sarzynski@gmail.com>
2018-12-23 12:32:59 +0100

    add date format test

--- spec/decorators/screening_decorator_spec.rb
+++ spec/decorators/screening_decorator_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ScreeningDecorator do
+  let(:screening) { FactoryBot.build_stubbed(:screening).decorate }
+
+  it 'returns proper screening date' do
+    expect(screening.screeningdate).to eq('2019-02-03 05:05')
+  end
 end
---
+++ spec/factories/screening.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :screening do |f|
+    f.film_id { 1 }
+    f.customer_id { 1 }
+    f.screeningdate { "2019-02-03T04:05:06+00:00" }
+    f.price { 12 }
+end
+end
