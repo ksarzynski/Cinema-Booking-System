8f392f49e75d8de947e2285747104e91da70a9bd
sarzynski <kamil.s.sarzynski@gmail.com>
2018-12-27 19:44:29 +0100

    change active admin user view

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,3 +1,26 @@
 ActiveAdmin.register Customer do
   permit_params :firstname, :secondname, :email
+
+  index do
+    selectable_column
+    id_column
+    column :firstname
+    column :secondname
+    column :email
+    actions
+  end
+
+  filter :firstname
+  filter :secondname
+  filter :email
+
+  form do |f|
+    f.inputs do
+      f.input :firstname
+      f.input :secondname
+      f.input :email
+    end
+    f.actions
+  end
+
 end
