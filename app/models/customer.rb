4a1efa46ea45a900bac781996b3582c36d94258c
sarzynski <kamil.s.sarzynski@gmail.com>
2018-12-29 23:07:51 +0100

    add uniqueness to customer validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   has_many :screenings, dependent: :destroy
   validates :firstname, length: { in: 2..30 }, format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/, message: 'has forbidden characters' }, presence: true
   validates :secondname, length: { in: 2..30 }, format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/, message: 'has forbidden characters' }, presence: true
-  validates :email, length: { in: 3..30 }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: 'is not valid' }, presence: true
+  validates :email, length: { in: 3..30 }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: 'is not valid' }, uniqueness: true, presence: true
 
   def self.search(search)
     where('firstname LIKE ? OR secondname LIKE ? OR email LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
